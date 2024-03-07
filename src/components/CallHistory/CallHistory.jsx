aac6341eafc89cace9564114858d7eb81dd4b95d
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-03-07 20:52:19 +0300

    Implemented call history interface with AntD

--- src/components/CallHistory/CallHistory.jsx
+++ src/components/CallHistory/CallHistory.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
+import { observer } from "mobx-react-lite";
+import { Table, Tag } from "antd";
+import { StoreContext } from "../../contexts/StoreContext";
 
-function CallHistory() {
-  return <div>CallHistory</div>;
-}
+const CallHistory = observer(() => {
+  const { callStore } = useContext(StoreContext);
+
+  const columns = [
+    {
+      title: "Тип",
+      dataIndex: "type",
+      key: "type",
+      render: (type) => (
+        <Tag color={type === "Входящий" ? "blue" : "green"}>{type}</Tag>
+      ),
+    },
+    {
+      title: "Номер",
+      dataIndex: "phoneNumber",
+      key: "phoneNumber",
+    },
+    {
+      title: "Дата",
+      dataIndex: "date",
+      key: "date",
+    },
+    {
+      title: "Длительность",
+      dataIndex: "duration",
+      key: "duration",
+    },
+    {
+      title: "Результат",
+      dataIndex: "result",
+      key: "result",
+      render: (result) => {
+        let color = result === "Не получилось" ? "volcano" : "green";
+        return <Tag color={color}>{result}</Tag>;
+      },
+    },
+  ];
+
+  return (
+    <div>
+      <h2>История звонков</h2>
+      <Table columns={columns} dataSource={callStore.callHistory} />
+    </div>
+  );
+});
 
 export default CallHistory;
