addd77d9fb503bdc29c8bb159dc5f59ddbaab6d9
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-03-07 18:00:15 +0300

    Added registration form to the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+//components
+import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  );
 }
 
 export default App;
