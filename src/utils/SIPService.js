555514bd84e150aa0e88e26cf0951d308e5ec831
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-03-07 19:10:53 +0300

    Added call handling functions

--- src/utils/SIPService.js
+++ src/utils/SIPService.js
@@ -1,6 +1,11 @@
 import JsSIP from "jssip";
+import callStore from "../stores/CallStore";
+import { EventEmitter } from "events";
 
 let ua;
+const sipEventEmitter = new EventEmitter();
+
+export const getSipEventEmitter = () => sipEventEmitter;
 
 export const initializeSIP = (
   uri,
@@ -24,5 +29,61 @@ export const initializeSIP = (
   ua.on("registered", onRegistered);
   ua.on("registrationFailed", onRegistrationFailed);
 
+  ua.on("newRTCSession", (data) => {
+    const { session } = data;
+    const number = session.local_identity.uri.user;
+
+    if (session.direction === "incoming") {
+      callStore.setCallSession(session, number, "incoming");
+    } else {
+      callStore.setCallStatus("Звоним...");
+    }
+
+    session.on("confirmed", () => {
+      callStore.setCallStart();
+    });
+    session.on("ended", () => {
+      callStore.addToCallHistory("Завершился");
+      setTimeout(() => {
+        callStore.setCallStatus("");
+      }, 3000);
+    });
+    session.on("failed", () => {
+      callStore.addToCallHistory("Не получилось");
+      setTimeout(() => {
+        callStore.setCallStatus("");
+      }, 3000);
+    });
+  });
+
   ua.start();
+};
+
+export const makeCall = (number) => {
+  const domain = String(ua.configuration.uri).split("@")[1];
+  const callUri = `sip:${number.trim()}@${domain}`;
+
+  const options = {
+    mediaConstraints: { audio: true, video: false },
+  };
+
+  const session = ua.call(callUri, options);
+
+  session.connection.addEventListener("addstream", (event) => {
+    sipEventEmitter.emit("addStream", event.stream);
+  });
+
+  callStore.setCallSession(session, number, "outgoing");
+};
+
+export const answerCall = (session) => {
+  session.answer();
+
+  session.connection.addEventListener("addstream", (event) => {
+    sipEventEmitter.emit("addStream", event.stream);
+  });
+};
+
+export const endCall = (session) => {
+  session.terminate();
 };
