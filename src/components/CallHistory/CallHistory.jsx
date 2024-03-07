0e5664ad543f96e657e88811c0108d6f12a04993
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-03-07 22:14:22 +0300

    Made call history look better on smaller screens

--- src/components/CallHistory/CallHistory.jsx
+++ src/components/CallHistory/CallHistory.jsx
@@ -2,49 +2,58 @@ import React, { useContext } from "react";
 import { observer } from "mobx-react-lite";
 import { Table, Tag } from "antd";
 import { StoreContext } from "../../contexts/StoreContext";
+import styles from "./CallHistory.module.scss";
 
 const CallHistory = observer(() => {
   const { callStore } = useContext(StoreContext);
 
   const columns = [
     {
-      title: "Тип",
-      dataIndex: "type",
-      key: "type",
-      render: (type) => (
-        <Tag color={type === "Входящий" ? "blue" : "green"}>{type}</Tag>
+      title: "Информация",
+      key: "info",
+      render: (record) => (
+        <>
+          <div>
+            <strong>Тип:</strong> {record.type}
+          </div>
+          <div>
+            <strong>Номер:</strong> {record.phoneNumber}
+          </div>
+          <div>
+            <strong>Дата:</strong> {record.date}
+          </div>
+        </>
       ),
     },
     {
-      title: "Номер",
-      dataIndex: "phoneNumber",
-      key: "phoneNumber",
-    },
-    {
-      title: "Дата",
-      dataIndex: "date",
-      key: "date",
-    },
-    {
-      title: "Длительность",
-      dataIndex: "duration",
-      key: "duration",
-    },
-    {
-      title: "Результат",
-      dataIndex: "result",
-      key: "result",
-      render: (result) => {
-        let color = result === "Не получилось" ? "volcano" : "green";
-        return <Tag color={color}>{result}</Tag>;
-      },
+      title: "Детали",
+      key: "details",
+      render: (record) => (
+        <>
+          <div>
+            <strong>Длительность:</strong> {record.duration}
+          </div>
+          <div>
+            <strong>Результат:</strong>{" "}
+            <Tag
+              color={record.result === "Не получилось" ? "volcano" : "green"}
+            >
+              {record.result}
+            </Tag>
+          </div>
+        </>
+      ),
     },
   ];
 
   return (
-    <div>
+    <div className={styles.callHistoryTable}>
       <h2>История звонков</h2>
-      <Table columns={columns} dataSource={callStore.callHistory} />
+      <Table
+        columns={columns}
+        dataSource={callStore.callHistory}
+        pagination={{ pageSize: 3, showSizeChanger: false }}
+      />
     </div>
   );
 });
