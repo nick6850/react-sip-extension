dbf2edda70dfa566643f9a8e14e0d4ec37681404
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-03-07 21:17:49 +0300

    Used AntD to enhance loading spinner

--- src/components/Common/Loading/Loading.jsx
+++ src/components/Common/Loading/Loading.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Spin } from "antd";
 
-const Loading = () => <div>Loading...</div>;
+const Loading = () => (
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+    }}
+  >
+    <Spin size="large" tip="Загрузка..." />
+  </div>
+);
 
 export default Loading;
