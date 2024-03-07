38f830ec4d1d5cf511953d751b98b4e33b5f13ad
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-03-07 21:13:50 +0300

    Enhanced styles with AntD library

--- src/components/IncomingCall/IncomingCall.jsx
+++ src/components/IncomingCall/IncomingCall.jsx
@@ -1,23 +1,44 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
+import { Modal, Button } from "antd";
+import { PhoneOutlined, CloseCircleOutlined } from "@ant-design/icons";
 import useIncomingCall from "../../hooks/useIncomingCall";
 
 const IncomingCall = observer(() => {
   const { handleAnswer, handleRefuse, audioRef, callStore } = useIncomingCall();
 
   return (
-    callStore.direction === "Входящий" &&
-    callStore.callStatus === "Входящий звонок" && (
-      <div>
-        <div>Входящий звонок</div>
-        <div>{callStore.contactNumber}</div>
-        <button onClick={handleAnswer}>Принять</button>
-        <button onClick={handleRefuse}>Сбросить</button>
-        <audio ref={audioRef} loop>
-          <source src="assets/ringtone.mp3" type="audio/mp3" />
-        </audio>
-      </div>
-    )
+    <>
+      <Modal
+        title="Входящий звонок"
+        open={
+          callStore.direction === "Входящий" &&
+          callStore.callStatus === "Входящий звонок"
+        }
+        onOk={handleAnswer}
+        onCancel={handleRefuse}
+        okText={
+          <>
+            <PhoneOutlined /> Принять
+          </>
+        }
+        cancelText={
+          <>
+            <CloseCircleOutlined /> Сбросить
+          </>
+        }
+        okButtonProps={{ type: "primary" }}
+        cancelButtonProps={{ type: "danger" }}
+        style={{ maxWidth: 300 }}
+      >
+        <p
+          style={{ fontSize: "1.5em", margin: 0 }}
+        >{`Звонок от ${callStore.contactNumber}`}</p>{" "}
+      </Modal>
+      <audio ref={audioRef} loop>
+        <source src="assets/ringtone.mp3" type="audio/mp3" />
+      </audio>
+    </>
   );
 });
 
