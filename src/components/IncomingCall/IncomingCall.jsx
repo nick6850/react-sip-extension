5538a8002a864081c7fc2ada00ac86b914485ffa
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-03-07 19:42:42 +0300

    Created interface for incoming calls

--- src/components/IncomingCall/IncomingCall.jsx
+++ src/components/IncomingCall/IncomingCall.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { observer } from "mobx-react-lite";
+import useIncomingCall from "../../hooks/useIncomingCall";
 
-function IncomingCall() {
-  return <div>IncomingCall</div>;
-}
+const IncomingCall = observer(() => {
+  const { handleAnswer, handleRefuse, audioRef, callStore } = useIncomingCall();
+
+  return (
+    callStore.direction === "incoming" &&
+    callStore.callStatus === "Входящий звонок" && (
+      <div>
+        <div>Входящий звонок</div>
+        <div>{callStore.contactNumber}</div>
+        <button onClick={handleAnswer}>Принять</button>
+        <button onClick={handleRefuse}>Сбросить</button>
+        <audio ref={audioRef} loop>
+          <source src="assets/ringtone.mp3" type="audio/mp3" />
+        </audio>
+      </div>
+    )
+  );
+});
 
 export default IncomingCall;
