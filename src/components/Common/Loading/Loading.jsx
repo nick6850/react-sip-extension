f3367393489b60c1bd08b4e9bedda38195a99a3a
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-03-08 03:02:42 +0300

    Changed color and size of the spinner

--- src/components/Common/Loading/Loading.jsx
+++ src/components/Common/Loading/Loading.jsx
@@ -2,15 +2,9 @@ import React from "react";
 import { Spin } from "antd";
 
 const Loading = () => (
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-    }}
-  >
+  <div>
     <Spin size="large" tip="Загрузка..." />
+    <span style={{ color: "white", marginLeft: "15px" }}>Загрузка</span>
   </div>
 );
 
