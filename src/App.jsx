33ab2d1f82f7df6e95254865186d86e349431b37
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-03-08 21:42:19 +0300

    Added logic to handle mic permission if disabled

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
 import { observer } from "mobx-react-lite";
-import { StoreContext, StoreProvider } from "./contexts/StoreContext";
+import { StoreContext } from "./contexts/StoreContext";
 
 import Loading from "./components/Common/Loading/Loading";
 import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
@@ -13,6 +13,7 @@ import styles from "./App.module.scss";
 const App = observer(() => {
   const { userStore, callStore } = useContext(StoreContext);
   const [showHistory, setShowHistory] = useState(false);
+  const [micPermission, setMicPermission] = useState("default");
 
   const handleLogout = () => {
     userStore.logOut();
@@ -30,39 +31,59 @@ const App = observer(() => {
     }
   }, [callStore.callStatus]);
 
+  useEffect(() => {
+    navigator.permissions.query({ name: "microphone" }).then((result) => {
+      setMicPermission(result.state);
+
+      result.onchange = () => {
+        setMicPermission(result.state);
+      };
+    });
+  }, []);
+
+  const openExtensionSettings = () => {
+    if (chrome && chrome.tabs && chrome.runtime) {
+      chrome.tabs.create({
+        url: `chrome://settings/content/siteDetails?site=chrome-extension://${chrome.runtime.id}`,
+      });
+    }
+  };
+
   return (
-    <StoreProvider>
-      <main>
-        <img src="assets/logo.jpg" alt="logo" className={styles.logo} />
-        {userStore.isSIPConnecting ? (
-          <div className={styles.loading}>
-            <Loading />
-          </div>
-        ) : !userStore.isRegistered ? (
-          <RegistrationForm />
-        ) : (
-          <>
-            <button onClick={handleLogout} className={styles.logoutButton}>
-              Выйти
-            </button>
-            {showHistory ? (
-              <CallHistory />
-            ) : (
-              <>
-                <IncomingCall />
-                <PhoneInterface />
-              </>
-            )}
-            <button
-              onClick={toggleHistory}
-              className={styles.showHistoryButton}
-            >
-              {showHistory ? "Назад к звонкам" : "Показать историю"}
-            </button>
-          </>
-        )}
-      </main>
-    </StoreProvider>
+    <main>
+      <img src="assets/logo.jpg" alt="logo" className={styles.logo} />
+      {micPermission !== "granted" ? (
+        <div className={styles.micPermission}>
+          <p>
+            Расширению для корректной работы необходим доступ к вашему микрофону
+          </p>
+          <button onClick={openExtensionSettings}>Открыть настройки</button>
+        </div>
+      ) : userStore.isSIPConnecting ? (
+        <div className={styles.loading}>
+          <Loading />
+        </div>
+      ) : !userStore.isRegistered ? (
+        <RegistrationForm />
+      ) : (
+        <>
+          <button onClick={handleLogout} className={styles.logoutButton}>
+            Выйти
+          </button>
+          {showHistory ? (
+            <CallHistory />
+          ) : (
+            <>
+              <IncomingCall />
+              <PhoneInterface />
+            </>
+          )}
+          <button onClick={toggleHistory} className={styles.showHistoryButton}>
+            {showHistory ? "Назад к звонкам" : "Показать историю"}
+          </button>
+        </>
+      )}
+    </main>
   );
 });
 
