1f6d72d6176de3e7847e72026073a12b315546f1
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-03-11 01:07:43 +0300

    Added port validation logic

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -4,6 +4,8 @@ import { initializeSIP } from "../utils/SIPService";
 class UserStore {
   isRegistered = false;
   isSIPConnecting = false;
+  connectionFailed = false;
+  registrationFailed = false;
 
   userDetails = {
     name: "",
@@ -31,30 +33,41 @@ class UserStore {
   }
 
   setUserDetails({ name, server, port, user, password }) {
+    this.resetUserStore();
+
     let serverAddress = server.startsWith("wss://")
       ? server
       : `wss://${server.trim()}`;
 
     let uri = `sip:${user.trim()}@${server.trim()}`;
 
+    // Port validation
     if (port) {
+      const portNum = parseInt(port, 10);
+      if (isNaN(portNum) || portNum < 1 || portNum > 65535) {
+        this.connectionFailed = true;
+        return;
+      }
       serverAddress += `:${port}`;
       uri += `:${port}`;
     }
 
-    // Update user details and save them to local storage
+    // Update user details
     this.userDetails = { name, serverAddress, uri, user, password };
-    localStorage.setItem("userDetails", JSON.stringify(this.userDetails));
+
     // Establish the SIP connection with the updated user details
     this.establishSIPConnection();
   }
 
-  logOut() {
+  resetUserStore() {
     // Clear user details and call history from localStorage
     localStorage.removeItem("userDetails");
     localStorage.removeItem("callHistory");
     // Reset store states
     this.isRegistered = false;
+    this.isSIPConnecting = false;
+    this.connectionFailed = false;
+    this.registrationFailed = false;
     this.userDetails = {
       name: "",
       serverAddress: "",
@@ -65,23 +78,37 @@ class UserStore {
   }
 
   establishSIPConnection() {
-    // Initialize SIP connection using stored URI and server address
     this.isSIPConnecting = true;
+    this.registrationFailed = false;
     initializeSIP(
       this.userDetails.uri,
       this.userDetails.serverAddress,
       this.userDetails.user,
       this.userDetails.password,
-      () => {
-        console.log("SIP registration succeeded");
-        this.isRegistered = true;
-        this.isSIPConnecting = false;
-      },
-      () => {
-        console.log("SIP registration failed");
-        this.isSIPConnecting = false;
-      }
+      this.handleConnectionFailed.bind(this),
+      this.handleRegistrationSuccess.bind(this),
+      this.handleRegistrationFailed.bind(this)
     );
+  }
+
+  handleConnectionFailed() {
+    console.log("SIP connection failed");
+    this.isSIPConnecting = false;
+    this.connectionFailed = true;
+  }
+
+  handleRegistrationFailed() {
+    console.log("SIP registration failed");
+    this.isSIPConnecting = false;
+    this.registrationFailed = true;
+  }
+
+  handleRegistrationSuccess() {
+    console.log("SIP registration succeeded");
+    this.isRegistered = true;
+    this.isSIPConnecting = false;
+
+    localStorage.setItem("userDetails", JSON.stringify(this.userDetails));
   }
 }
 
